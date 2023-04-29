94e2ca1b803f6cb6e2960fa73cd142f4f9e4febf
UAA <uaa04@yandex.ru>
2023-04-29 11:06:03 +0900

    fix

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import view.View;
 public class Main {
     public static void main(String[] args) {
 
-        Model model = new Model("final_work_2/files/");
+        Model model = new Model("/final_work_2/files/");
         View view = new ConsoleUI();
         new Presenter(view, model);
         view.start();
